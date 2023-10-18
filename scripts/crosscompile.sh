8145a7c7fa516454d6d4413ec81f1880a1c8cea3
matvey <matvey@copters.ru>
2023-10-18 03:11:54 +0300

    windows

--- scripts/crosscompile.sh
+++ scripts/crosscompile.sh
@@ -26,23 +26,23 @@ rm -f ${git_root_path}/binaries/latest;
 cd ${git_root_path}/binaries; ln -s ${version} latest; cd ${git_root_path}/scripts;
 
 for os in android aix darwin dragonfly freebsd illumos ios js linux netbsd openbsd plan9 solaris windows zos;
-  #for os in darwin;
+#for os in windows;
 do
   for arch in "amd64" "386" "arm" "arm64" "mips64" "mips64le" "mips" "mipsle" "ppc64" "ppc64le" "riscv64" "s390x" "wasm" 
   do
     target_os_name=${os}
-    [ "$os" == "windows" ] && execution_file="sitebrush.exe"
+    [ "$os" == "windows" ] && execution_file="sitebrush.exe" && build_flags="-H windowsgui "
     [ "$os" == "darwin" ] && target_os_name="mac"
 
  #compile gui app:
- GOOS=${os} GOARCH=${arch} go build -ldflags "-X sitebrush/pkg/config.CompileVersion=${version}" -o ${execution_file} ../sitebrush_gui.go 2> /dev/null
+ GOOS=${os} GOARCH=${arch} go build -ldflags "${build_flags}-X sitebrush/pkg/config.CompileVersion=${version}" -o ${execution_file} ../sitebrush_gui.go 2> /dev/null
 
  if [ "$?" == "0" ]
  then
    mkdir -p ../binaries/${version}/gui/${target_os_name}/${arch}
    chmod +x ${execution_file}
    mv ${execution_file} ../binaries/${version}/gui/${target_os_name}/${arch}/
-   echo "GOOS=${os} GOARCH=${arch} go build -ldflags "-X sitebrush/pkg/config.CompileVersion=${version}" -o ../binaries/${version}/gui/${target_os_name}/${arch}/${execution_file} ../sitebrush_gui.go"
+   echo "GOOS=${os} GOARCH=${arch} go build -ldflags "${build_flags}-X sitebrush/pkg/config.CompileVersion=${version}" -o ../binaries/${version}/gui/${target_os_name}/${arch}/${execution_file} ../sitebrush_gui.go"
  fi
 
     #compile non gui app:
